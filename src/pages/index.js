832b506f57f791d55a0779d08d8af4b3223aab10
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-07-01 17:55:55 +0200

    CSS module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from 'react'
+import Layout from '../components/layout'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <h1 className="title">This is our homepage</h1>
+    Hello world! Griaß enk Leitln! I bins, da Hansi!
+    <h1 className="title">Another heading.</h1>
+  </Layout>
+)
