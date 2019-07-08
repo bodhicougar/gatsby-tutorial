ba24d899da5a230fc78bc7b91539061ba9ce1081
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-07-08 16:26:51 +0200

    GraphQL setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,13 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`,
-  `gatsby-plugin-styled-components`],
+  siteMetadata: {
+    titel: 'tutorial',
+    description: 'gatsby tutorial via udemy',
+    author: '@bodhicougar',
+    data: ['item1', 'item2'],
+  },
+  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
