ae07ba2416dddee2c83b03608bfec074decd5403
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-07-11 20:59:15 +0200

    Text format

--- src/pages/images.js
+++ src/pages/images.js
@@ -2,11 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const images = () => {
-    return (
-        <Layout>
-            This is the images page
-        </Layout>
-    )
+  return <Layout>This is the images page</Layout>
 }
 
 export default images
