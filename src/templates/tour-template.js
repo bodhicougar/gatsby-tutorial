e69d8670e210dc3c4e56139284b6a0feb1b892c2
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-08-05 11:14:50 +0200

    Tour template fertig!

--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -6,12 +6,78 @@ import Image from 'gatsby-image'
 import { FaMoneyBillWave, FaMap } from 'react-icons/fa'
 import { Link } from 'gatsby'
 
-const tourTemplate = props => {
-  console.log(props)
+const TourTemplate = ({ data }) => {
+  const {
+    name,
+    price,
+    country,
+    days,
+    description: { description },
+    images,
+    start,
+  } = data.tour
 
-  return <div>Hello from your tour template</div>
+  return (
+    <Layout>
+      <section className={styles.template}>
+        <div className={styles.center}>
+          <div className={styles.images}>
+            {images.map((item, index) => {
+              return (
+                <Image
+                  key={index}
+                  fluid={item.fluid}
+                  alt={name}
+                  className={styles.image}
+                />
+              )
+            })}
+          </div>
+          <h2>{name}</h2>
+          <div className={styles.info}>
+            <p>
+              <FaMoneyBillWave className={styles.icon}></FaMoneyBillWave>
+              starting from ${price}
+            </p>
+            <p>
+              <FaMap className={styles.icon} />
+              {country}
+            </p>
+          </div>
+          <h4>starts on : {start}</h4>
+          <h4>duration : {days} days</h4>
+          <p className={styles.desc}>{description}</p>
+          <h2>Daily schedule</h2>
+          {/* <ul className={styles.journey}>
+            {journey.map((item, index) => {
+              return <li key={index}>{item.day}</li>
+            })}
+          </ul> */}
+          <Link to="/tours">back to tours</Link>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
-export const query = graphql
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: { eq: $slug }) {
+      name
+      price
+      country
+      days
+      start(formatString: "dddd MMMM Do, YYYY")
+      description {
+        description
+      }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
+    }
+  }
+`
 
-export default tourTemplate
+export default TourTemplate
